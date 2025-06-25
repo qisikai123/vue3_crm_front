034e4d670fa50b547ec14990a869501635c6ca9b
qisk <qisk@asiainfo.com>
2025-06-25 17:15:59 +0800

    &&

---
+++ src/components/ReI18nToggle/index.ts
@@ -0,0 +1,7 @@
+import reI18nToggle from "./src/index.vue";
+import { withInstall } from "@pureadmin/utils";
+
+/** 图形验证码组件 */
+export const ReI18nToggle = withInstall(reI18nToggle);
+
+export default ReI18nToggle;
